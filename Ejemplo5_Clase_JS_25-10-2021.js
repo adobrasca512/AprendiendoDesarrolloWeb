2cfc821d0ca3c06c99981c6eea3af322763f772a
Adilem Dobras Castillo <59998402+adobrasca512@users.noreply.github.com>
2021-10-25 12:19:23 +0200

    Add files via upload

---
+++ Ejemplo5_Clase_JS_25-10-2021.js
@@ -0,0 +1,24 @@
+console.log("Hola Mundo!")
+				let i
+				i=10, numero="10"
+				console.log("i es igual a: "+i)
+				if (i%2==0) {
+
+					console.log("i es par")
+				}
+				
+				if (i==numero) { 
+					console.log("son iguales")
+				}
+				else{
+					console.log("no son iguales")
+				}
+				for (var j = 1; j <= i; j++) {
+					console.log("contando: "+j)
+				
+				}
+				let vocales=['a','e','i','o']
+				for (var k = 0; k < vocales.length; k++) {
+					console.log("letras: "+vocales[k])
+				}
+				document.body.innerHTML='<h1>Desarrollo web<h1>'
